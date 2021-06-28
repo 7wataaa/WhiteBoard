d7e21b1eea4e52718a9330b83313acc83805744a
nashi <nn.contact.n@gmail.com>
2021-06-28 10:30:51 +0900

    docs: 処理についてのコメントを追加

--- app/src/main/java/com/example/whiteboard/IntroductionPageActivity.kt
+++ app/src/main/java/com/example/whiteboard/IntroductionPageActivity.kt
@@ -13,22 +13,34 @@ import  androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 
+//ページの総数
 private const val NUM_PAGES = 3
 
+/**
+ * 初回起動時に表示するintroductionページのActivity
+ */
 class IntroductionPageActivity : FragmentActivity() {
+    //初回の文字をいくつかのページに分けていい感じにする用viewPager
     private lateinit var viewPager: ViewPager2
+
+    //インジケータたちを画面に表示する場所
     private lateinit var indicatorArea: LinearLayout
+
+    //インジケータ実体たちが入ってるリスト
     private var indicatorViewList: ArrayList<View> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_introduction_page)
 
+        //viewPager
         viewPager = findViewById(R.id.pager)
 
+        //viewPagerのAdapterにIntroductionPageAdapterを設定
         val pagerAdapter = IntroductionPageAdapter(this)
         viewPager.adapter = pagerAdapter
 
+        //ページが切り替えられたときの処理を設定、object:foo()は、foo()オブジェクトで、{}の中でoverrideができる。すごい
         viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageScrolled(
                 position: Int,
@@ -36,6 +48,8 @@ class IntroductionPageActivity : FragmentActivity() {
                 positionOffsetPixels: Int
             ) {
                 super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+
+                //スクロールされたときに対応するインジケータの表示色を更新する
                 for (i in 0 until indicatorViewList.size) {
                     if (i == position)
                         indicatorViewList[i].background = ResourcesCompat.getDrawable(
@@ -53,15 +67,16 @@ class IntroductionPageActivity : FragmentActivity() {
             }
         })
 
-        println("area id = ${R.id.indicator_area}")
-
+        //インジケータを入れるとこ
         indicatorArea = findViewById(R.id.indicator_area)
 
+        //インジケータの値たちをファイルから取ってくる
         val indicatorWidth = resources.getDimension(R.dimen.walk_through_indicator_size).toInt()
         val indicatorHeight = resources.getDimension(R.dimen.walk_through_indicator_size).toInt()
         val indicatorMarginStart =
             resources.getDimension(R.dimen.walk_through_indicator_margin_start).toInt()
 
+        //onCreate時のデフォルトインジケーター表示をつくるfor
         for (i in 0..2) {
             val view = View(this)
             if (i == 0) {
@@ -83,6 +98,7 @@ class IntroductionPageActivity : FragmentActivity() {
     }
 
     override fun onBackPressed() {
+        //戻るボタンが押されたときの処理、currentItem == 0のときの動作は要検証、アプリを初めて開いたときに戻るボタン押下なのでホームに戻るようにしたい
         if (viewPager.currentItem == 0) {
             super.onBackPressed()
         } else {
@@ -90,6 +106,7 @@ class IntroductionPageActivity : FragmentActivity() {
         }
     }
 
+    //初回の文字をいくつかのページに分けていい感じにする用viewPagerで使用するPageAdapter、getItemCountとcreateFragmentをそれぞれ設定してるだけ
     private inner class IntroductionPageAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
         override fun getItemCount(): Int = NUM_PAGES
 
